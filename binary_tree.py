5714df727ddea2d75dbd2041fed5c21ae767f431
Andy Yao <acmerfight@gmail.com>
2013-12-06 00:07:03 +0800

    add judge binary is banlaced

--- binary_tree.py
+++ binary_tree.py
@@ -58,14 +58,39 @@ def get_tree_depth(root, depth=0):
     else:
         left_depth = get_tree_depth(root.left)
         right_depth = get_tree_depth(root.right)
-    return left_depth + 1 if left_depth > right_depth else right_depth + 1
+    return max(left_depth, right_depth) + 1 
 
+def is_banlanced(root, depth_info={}):
+    """ O(n)"""
+    if root is None:
+        depth_info["depth"] = 0
+        return True 
+    left_depth = {}
+    right_depth = {}
+    if is_banlanced(root.left, left_depth) and is_banlanced(root.right, right_depth):
+        if abs(left_depth["depth"] - right_depth["depth"]) <= 1:
+            depth_info["depth"] = max(left_depth["depth"], right_depth["depth"]) + 1
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def is_banlanced_one(root):
+    """ O(n2)"""
+    if root is None:
+        return True
+    if abs(get_tree_depth(root.left) - get_tree_depth(root.right)) > 1:
+        return False
+    else:
+        return is_banlanced_one(root.left) and is_banlanced_one(root.right)
+    
 
 
 if __name__ == "__main__":
     node1 = TreeNode(1)
     node2 = TreeNode(2)
-    node2.left = TreeNode(8)
+    #node2.left = TreeNode(8)
     node1.left = TreeNode(9)
     node1.right = TreeNode(10)
     root = TreeNode(0, node1, node2)
@@ -79,3 +104,5 @@ if __name__ == "__main__":
     #post_order(root)
 
     print get_tree_depth(root)
+    print is_banlanced(root)
+    print is_banlanced(root)
