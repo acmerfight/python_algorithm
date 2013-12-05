5122c4d7f22aa14073f0612d3400ceaa74c69ac0
acmerfight <acmerfight@gmail.com>
2013-12-05 18:52:10 +0800

    add tree depth

--- binary_tree.py
+++ binary_tree.py
@@ -1,3 +1,12 @@
+class stack(list):
+    def push(self, item):
+        self.append(item)
+
+    def is_empty(self):
+        return not self
+
+
+
 class TreeNode(object):
     def __init__(self, data=None, left=None, right=None):
         self.data = data
@@ -6,37 +15,50 @@ class TreeNode(object):
 
 
 def pre_order(root):
-    if root.data is None:
-        print "empty tree!"
+    if root is None:
+        pass
     else:
         print root.data
-        if root.left is not None:
-            pre_order(root.left)
-        if root.right is not None:
-            pre_order(root.right)
+        pre_order(root.left)
+        pre_order(root.right)
+
+def nonrecursive_pre_order(root):
+    s = stack()
+    s.push(root)
+    while not s.is_empty():
+        node = s.pop()
+        print node.data
+        if node.right is not None:
+            s.push(node.right)
+        if node.left is not None:
+            s.push(node.left)
 
 
 def in_order(root):
-    if root.data is None:
-        print "empty tree!"
+    if root is None:
+        pass
     else:
-        if root.left is not None:
-            in_order(root.left)
+        in_order(root.left)
         print root.data
-        if root.right is not None:
-            in_order(root.right)
+        in_order(root.right)
+
 
 def post_order(root):
-    if root.data is None:
-        print "empty tree!"
+    if root is None:
+        pass
     else:
-        if root.left is not None:
-            post_order(root.left)
-        if root.right is not None:
-            post_order(root.right)
+        post_order(root.left)
+        post_order(root.right)
         print root.data
 
 
+def get_tree_depth(root, depth=0):
+    if root is None:
+        return 0
+    else:
+        left_depth = get_tree_depth(root.left)
+        right_depth = get_tree_depth(root.right)
+    return left_depth + 1 if left_depth > right_depth else right_depth + 1
 
 
 
@@ -44,10 +66,16 @@ if __name__ == "__main__":
     node1 = TreeNode(1)
     node2 = TreeNode(2)
     node2.left = TreeNode(8)
+    node1.left = TreeNode(9)
+    node1.right = TreeNode(10)
     root = TreeNode(0, node1, node2)
-    print "pre_order"
-    pre_order(root)
-    print "in_order"
-    in_order(root)
-    print "post_order"
-    post_order(root)
+    #print "pre_order"
+    #pre_order(root)
+    #print "nonrecursive_pre_order"
+    #nonrecursive_pre_order(root)
+    #print "in_order"
+    #in_order(root)
+    #print "post_order"
+    #post_order(root)
+
+    print get_tree_depth(root)
