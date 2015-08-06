338e373e53f56fc689b2a979472a2ee57450e079
acmerfight <acmerfight@gmail.com>
2015-08-06 23:59:52 +0800

    add ohter solution

--- leetcode/Number_of_1_Bits.py
+++ leetcode/Number_of_1_Bits.py
@@ -11,3 +11,10 @@ class Solution:
                 val += 1
             n /= 2
         return val
+
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def hammingWeight(self, n):
+        return bin(n).count("1")
