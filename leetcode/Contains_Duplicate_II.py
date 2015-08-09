7a24da4eaefb9cc65a45f96c09e993fb5fbc576d
acmerfight <acmerfight@gmail.com>
2015-08-09 23:43:25 +0800

    add Contains_Duplicate_II.py

---
+++ leetcode/Contains_Duplicate_II.py
@@ -0,0 +1,10 @@
+
+# https://leetcode.com/problems/contains-duplicate-ii/
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {boolean}
+    def containsNearbyDuplicate(self, nums, k):
+        pass
