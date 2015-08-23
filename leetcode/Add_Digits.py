0e1d0178738e94fa46cc8cb81a9a49c4834d5fa3
acmerfight <acmerfight@gmail.com>
2015-08-23 23:06:03 +0800

    complete Add_Digits.py

--- leetcode/Add_Digits.py
+++ leetcode/Add_Digits.py
@@ -7,4 +7,9 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        pass
+        num = str(num)
+        num_int = sum([int(i) for i in num])
+        if len(str(num_int)) == 1:
+            return num_int
+        else:
+            return self.addDigits(num_int)
