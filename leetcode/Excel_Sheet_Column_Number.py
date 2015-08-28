79c8ed1a4f7bf94b4dda2262bfafdd851e7c664f
acmerfight <acmerfight@gmail.com>
2015-08-28 22:59:16 +0800

    add Excel_Sheet_Column_Number.py

---
+++ leetcode/Excel_Sheet_Column_Number.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/excel-sheet-column-number/
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
