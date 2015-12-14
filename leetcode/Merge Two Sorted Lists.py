9891d6bc75fd9a9f8a06c6063e884808e4736a05
acmerfight <acmerfight@gmail.com>
2015-12-14 23:30:14 +0800

    add merge two list

--- leetcode/Merge Two Sorted Lists.py
+++ leetcode/Merge Two Sorted Lists.py
@@ -17,3 +17,29 @@ class Solution:
         else:
             current_node.next = l2
         return dummy.next
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        cur = ListNode(None)
+        head = cur
+        while l1 is not None and l2 is not None:
+            if l1.val < l2.val:
+                cur.next = l1
+                cur = cur.next
+                l1 = l1.next
+            else:
+                cur.next = l2
+                cur = cur.next
+                l2 = l2.next
+        cur.next = l1 if l1 else l2
+        return head.next
