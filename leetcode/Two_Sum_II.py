87a29716c98a8f9490b0b2cc577a454202245337
acmerfight <acmerfight@gmail.com>
2016-09-20 22:35:27 +0800

    add Two_Sum_II.py

---
+++ leetcode/Two_Sum_II.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(numbers) < 2:
+            return []
+        i = 0
+        j = len(numbers) - 1
+        while i < j:
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            elif numbers[i] + numbers[j] == target:
+                return i + 1, j + 1
