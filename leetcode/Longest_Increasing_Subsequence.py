368537b9e6e3c55a1333e7eaba70130106a4aadb
acmerfight <acmerfight@gmail.com>
2018-01-01 23:19:03 +0800

    add another solution to Longest_Increasing_Subsequence

--- leetcode/Longest_Increasing_Subsequence.py
+++ leetcode/Longest_Increasing_Subsequence.py
@@ -14,3 +14,35 @@ class Solution(object):
             if dp[i] > res:
                 res = dp[i]
         return res
+
+
+
+class Solution(object):
+
+    @staticmethod
+    def _insert_number(data, number):
+        if number > data[-1]:
+            data.append(number)
+            return
+        start = 0
+        end = len(data) - 1
+        while start < end:
+            mid = start + (end - start) / 2
+            if number > data[mid]:
+                start = mid + 1
+            else:
+                end = mid
+        data[end] = number
+
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        data = [nums[0]]
+        for num in nums[1:]:
+            self._insert_number(data, num)
+        return len(data)
+
